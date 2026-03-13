065585e4872d32f1c4c3a610972c4ce7e842f798
Vishal-047 <kingrw007@gmail.com>
2026-03-13 17:07:34 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // @ts-expect-error: NextConfig type does not include eslint in newer Next.js versions, but Next.js build uses it still
   eslint: {
     ignoreDuringBuilds: true,
   },
